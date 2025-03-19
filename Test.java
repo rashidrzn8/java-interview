c39bdf6c1e419452e770b882a22b00684be7044f
rashid <rashidrizan8@gamil.com>
2025-03-19 23:24:24 +0530

    fwdjfniwe

--- Test.java
+++ Test.java
@@ -1,5 +1,14 @@
 public class Test {
     public static void main(String[] args) {
-
+//        String demo = "My name is Rashid";
+//        char[] charr = demo.toCharArray();
+//        String result = "";
+//
+//        for (int i =0 ; i< demo.length();i++){
+//            if (charr[i] != ' '){
+//                result= result+charr[i];
+//            }
+//        }
+//        System.out.println(result);
     }
 }
