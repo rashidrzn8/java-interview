0ad4b5afaea8b985009f824a3fbc8369cabe7e12
rashid <rashidrizan8@gamil.com>
2025-03-20 15:22:56 +0530

    sdnsjd

--- Test.java
+++ Test.java
@@ -116,6 +116,7 @@ public class Test {
         for (int i=0;i<arr.length-1;i++){
             if(arr[i] != arr[i+1]){
                 temp[j++]=arr[i];
+                temp[j++]=arr[i];
             }
         }
         temp[j++] = arr[arr.length-1];
